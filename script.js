bf7925f6666d10345477a931ea50211dc6fda30c
Ekaterina Zarudnaya <zarudnayakatyaa@gmail.com>
2021-02-09 11:07:02 +0200

    Изменения в проварках if на нажатый символ

--- script.js
+++ script.js
@@ -24,35 +24,32 @@ function insert(num) {
         p_header.innerHTML=num;
     }
 
-    do {
-        arr_histor_num.push(num);
+    arr_histor_num.push(num);
 
-        if(isNaN(arr_histor_num[arr_histor_num.length-1])==true /*&& isNaN(arr_histor_num[arr_histor_num.length-2])==true*/){
-            alert("1 if");
-            /*delete arr_histor_num[arr_histor_num.length-2];*/
-        }
-        
-        p_histor.innerHTML=arr_histor_num.join('');
+    if(isNaN(arr_histor_num[arr_histor_num.length-1])==true /*&& isNaN(arr_histor_num[arr_histor_num.length-2])==true*/){
+        alert("1 if");
+        /*delete arr_histor_num[arr_histor_num.length-2];*/
+    }
+    
+    p_histor.innerHTML=arr_histor_num.join('');
 
-        let in_line_res = eval(p_histor.innerHTML);
+    let in_line_res = eval(p_histor.innerHTML);
 
-        if (in_line_res) {
-            p_result.innerHTML= in_line_res;
-        }
+    if (in_line_res) {
+        p_result.innerHTML= in_line_res;
+    }
 
-        if(isNaN(arr_histor_num[arr_histor_num.length-1])==true /*&& arr_histor_num[arr_histor_num.length-3]=="="*/){
-            alert("2 if");
-            /*arr_histor_num=[];
-            p_histor.innerHTML=in_line_res;*/
-        }
+    if(isNaN(arr_histor_num[arr_histor_num.length-1])==true /*&& arr_histor_num[arr_histor_num.length-3]=="="*/){
+        alert("2 if");
+        /*arr_histor_num.splice(0,arr_histor_num.length-2);
+        p_histor.innerHTML=in_line_res;*/
+    }
 
-        if(/*arr_histor_num[arr_histor_num.length-3]=="="&&*/ isNaN(arr_histor_num[arr_histor_num.length-1])==false){
-            alert("3 if");
-           /* arr_histor_num.splice(0,length-2);
-            p_histor.innerHTML=arr_histor_num.join('');*/
-        }
-
-    } while (false);
+    if(/*arr_histor_num[arr_histor_num.length-3]=="="&&*/ isNaN(arr_histor_num[arr_histor_num.length-1])==false){
+        alert("3 if");
+        /* arr_histor_num.splice(0,arr_histor_num.length-1);
+        p_histor.innerHTML=arr_histor_num.join('');*/
+    }
     
 }
 
@@ -64,6 +61,13 @@ function calc(){
 
         arr_histor_num.push("=");
         arr_histor_num.push(value);
+        p_header.innerHTML=0;
+
+        let arr = [1,2,3,4,5,6];
+
+        arr.splice(0,arr.length-2);
+
+        alert(arr.join(''));
 
         p_histor.innerHTML=p_histor.innerHTML+"="+value;
 
